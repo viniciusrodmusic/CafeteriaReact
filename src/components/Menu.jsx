dafdcb5d3e72104afd451265ccaf4b8dc12dcc8b
viniciusrodmusic <aviniciusrodrigu@gmail.com>
2025-10-21 20:14:58 -0300

    fix: adicionei gap no mobile entre as opcoes de cafe

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,7 +10,7 @@ function Menu() {
             <h2 className="text-4xl lg:text-6xl text-white text-center break-normal w-3/4">How would you like to <span className="text-[var(--light-coffee)]">feel</span> today?</h2>
 
             {/* A BANDEJA DE COR CLARA COM AS OPÇÕES DE CAFÉ DENTRO: */}
-            <div className="p-8 gap-4 bg-[var(--lighter-coffee)] size-full xl:size-3/4 rounded-4xl flex-col xl:flex-row flex justify-around items-center" >
+            <div className="p-8 gap-[100px] sm:gap-6 bg-[var(--lighter-coffee)] size-full xl:size-3/4 rounded-4xl flex-col xl:flex-row flex justify-around items-center" >
                 <CoffeeOption textContent="sweet with Lapuccino + Chocolate" coffeeImage="3" price="$ 5.00"/>
                 <CoffeeOption textContent="stronger with Black Coffee" coffeeImage="1" price="$ 2.00"/>
                 <CoffeeOption textContent="just calm with Coffee + Milk" coffeeImage="2" price="$ 3.00"/>
