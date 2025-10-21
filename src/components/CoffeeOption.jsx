75844ba5b59ba9d3dea61606bd589367bd0ad886
viniciusrodmusic <aviniciusrodrigu@gmail.com>
2025-10-21 19:20:27 -0300

    fix: tentando corrigir distorcao na secao menu

--- src/components/CoffeeOption.jsx
+++ src/components/CoffeeOption.jsx
@@ -31,22 +31,22 @@ function CoffeeOption({coffeeImage, textContent, price}) {
     useState
 
     return (
+        // CONTAINER QUE ABRIGA O CARD
         <section ref={containerRef} className={`
         ${isVisible ? "transform-[translateY(0px)] opacity-100" : "transform-[translateY(50px)] opacity-0"}
         hover:outline-1 hover:shadow-[1px_1px_50px_rgb(255,200,150)] 
         cursor-pointer duration-1500 rounded-3xl text-white bg-linear-to-bl from-[var(--dark-coffee)] 
         to-[var(--dark-coffee2)] flex flex-col items-center justify-around overflow-hidden 
-        shadow-[-10px_10px_40px_rgba(0,0,0,0.5)] p-2 w-3/4 lg:h-4/4 lg:w-2/4`}>
-
+        shadow-[-10px_10px_40px_rgba(0,0,0,0.5)] p-2 lg:h-4/4 lg:w-2/4`}>
+            {/* IMAGEM DO CARD */}
             <img src={coffeeImageUrl} className="p-4 rounded-4xl aspect-square h-2/4 sm:h-64 xl:h-64 object-cover"></img>
-
+            {/* DESCRIÇÃO DO CAFÉ */}
             <p className=" h-24 flex text-center items-center font-bold text-[min(4vw,1.4rem)]">{textContent}</p>
 
+            {/* CONTAINER QUE ABRIGA O PREÇO E O BOTÃO DE COMPRAR */}
             <div className="flex items-center justify-around h-24 w-full whitespace-nowrap">
-
                 <p className="font-['DM_Serif_Display',Arial] text-[min(5vw,2rem)] text-[var(--light-coffee)]">{price}</p>
                 <Button bgColor="yellow" textColor="black" textContent="order"/>
-
             </div>
 
         </section>
