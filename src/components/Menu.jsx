88df20ad2f1e439cf7e7e4083a0ba97745554a01
viniciusrodmusic <aviniciusrodrigu@gmail.com>
2025-10-21 19:40:45 -0300

    fix: tentando corrigir distorcao 3

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,9 +5,11 @@ function Menu() {
 
 
     return (
-        <section className="h-[300vh] xl:h-screen bg-[var(--dark-coffee)] flex flex-col justify-start md:justify-around items-center xl:p-8 gap-4" >
+        <section className="h-auto xl:h-screen bg-[var(--dark-coffee)] flex flex-col justify-start md:justify-around items-center xl:p-8 gap-4" >
 
             <h2 className="text-4xl lg:text-6xl text-white text-center break-normal w-3/4">How would you like to <span className="text-[var(--light-coffee)]">feel</span> today?</h2>
+
+            {/* A BANDEJA DE COR CLARA COM AS OPÇÕES DE CAFÉ DENTRO: */}
             <div className="p-8 gap-4 bg-[var(--lighter-coffee)] size-full xl:size-3/4 rounded-4xl flex-col xl:flex-row flex justify-around items-center" >
                 <CoffeeOption textContent="sweet with Lapuccino + Chocolate" coffeeImage="3" price="$ 5.00"/>
                 <CoffeeOption textContent="stronger with Black Coffee" coffeeImage="1" price="$ 2.00"/>
